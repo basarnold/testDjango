6399f13adf71b17b16db835d8ef6656bd1f9a056
b.arnold <basarnold1@gmail.com>
2025-07-06 14:58:59 +0200

    Nieuw model 'Voetbalspelers' is toegevoegd

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -11,11 +11,25 @@ class Post(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
 
 
-def publish():
+def publish(self):
     self.published_date = timezone.now()
     self.save()
 
 
 def __str__(self):
     return self.title
+
+
+class Voetbalspelers(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name_player = models.CharField(max_length=50)
+    name_club = models.CharField(max_length=50)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+def publish(self):
+    self.published_data = timezone.now()
+    self.save()
+
+
 # Create your models here.
